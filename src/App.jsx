1081678a3ad102efa952e4c29d1c2d1a7454ca1b
m3rcury02 <gunal2002@gmail.com>
2023-08-13 01:40:14 +0530

    implemented states

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from "react";
 
 export default function App() {
-  useState("")
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
+    });
+    setNewItem("")
+  }
+
   return (
-    <div className="m-5">
-      <form className="flex flex-col gap-2 mb-5">
+    <div className="m-5 w-52">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-2 mb-5">
         <div className="flex flex-col gap-1 ">
           <label htmlFor="item">New Item</label>
           <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
             type="text"
             id="item"
             className="rounded-sm bg-blue-500 text-white p-1"
@@ -19,33 +34,19 @@ export default function App() {
       </form>
       <h1 className=" text-2xl">Todo List</h1>
       <ul className="ml-3 text-lg">
-        <li className="mt-2">
-          <label>
-            <input type="checkbox" className="mr-2" />
-            Item 1
-          </label>
-          <button className="ms-2 bg-opacity-25 bg-red-600 border-2 border-red-500 rounded-md p-1 px-2">
-            Delete
-          </button>
-        </li>
-        <li className="mt-2">
-          <label>
-            <input type="checkbox" className="mr-2" />
-            Item 2
-          </label>
-          <button className="ms-2 bg-opacity-25 bg-red-600 border-2 border-red-500 rounded-md p-1 px-2">
-            Delete
-          </button>
-        </li>
-        <li className="mt-2">
-          <label>
-            <input type="checkbox" className="mr-2" />
-            Item 3
-          </label>
-          <button className="ms-2 bg-opacity-25 bg-red-600 border-2 border-red-500 rounded-md p-1 px-2">
-            Delete
-          </button>
-        </li>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id} className="mt-2">
+              <label>
+                <input type="checkbox" className="mr-2" checked={todo.completed} />
+                {todo.title}
+              </label>
+              <button className="ms-2 bg-opacity-25 bg-red-600 border-2 border-red-500 rounded-md p-1 px-2">
+                Delete
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
